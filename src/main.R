9ac9e6a439b2b334ce6303042edc39403497ed02
Kunaal Desai <kunaaldesai13@gmail.com>
2025-09-17 16:04:56 -0500

    Added more positions

--- src/main.R
+++ src/main.R
@@ -5,6 +5,12 @@ source("src/scripts/02_clean_names.R")
 source("src/scripts/03_qb_season_aggregates.R")
 source("src/scripts/04_qb_weekly_aggregates.R")
 source("src/scripts/05_caleb_analysis.R")
+source("src/scripts/06_wr_season_aggregates.R")
+#source("src/scripts/07_wr_weekly_aggregates.R")
+#source("src/scripts/08_rb_season_aggregates.R")
+#source("src/scripts/09_rb_weekly_aggregates.R")
+
+
 
 
 message("Pipeline complete ✅")
---
+++ src/scripts/06_wr_season_aggregates.R
@@ -0,0 +1,24 @@
+# ===== Season WR aggregates (REG only) + merge adv =====
+
+wr_25 <- pbp_25 %>%
+  filter(season_type == "REG") %>%
+  group_by(id, receiver, posteam) %>%
+  summarize(
+    epa_play = mean(epa, na.rm = TRUE),
+    total_epa = sum(epa, na.rm = TRUE),
+    yac_epa = mean(yac_epa, na.rm= TRUE),
+    total_yac_epa = sum(yac_epa, na.rm = TRUE),
+    completed_yac_epa = mean(comp_yac_epa, na.rm = TRUE),
+    total_completed_yac_epa = sum(comp_yac_epa, na.rm = TRUE),
+    xyac_epa = mean(xyac_epa, na.rm = TRUE),
+    total_xyac_epa = sum(xyac_epa, na.rm = TRUE),
+    xyac_mean_yardage = mean(xyac_mean_yardage, na.rm = TRUE),
+    total_xyac_mean_yardage = sum(xyac_mean_yardage, na.rm = TRUE),
+    xyac_median_yardage	= mean(xyac_median_yardage, na.rm = TRUE),
+    total_xyac_median_yardage	= mean(xyac_median_yardage, na.rm = TRUE),
+    total_receiving_yards = sum(receiving_yards, na.rm = TRUE),
+    total_yards_gained = sum(yards_gained, na.rm = TRUE),
+    total_yac = sum(yards_after_catch, na.rm = TRUE),
+    avg_yac = mean(yards_after_catch, na.rm = TRUE),
+  ) %>%
+  arrange(desc(epa_play))
